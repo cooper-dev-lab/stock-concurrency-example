a797a1f7eb7c7bb36648d1c355847854af895260
Cooper <pbg0205@naver.com>
2022-10-12 12:22:37 +0900

    first commit

---
+++ src/main/java/com/cooper/stockconcurrencyexample/StockConcurrencyExampleApplication.java
@@ -0,0 +1,13 @@
+package com.cooper.stockconcurrencyexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockConcurrencyExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockConcurrencyExampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cooper/stockconcurrencyexample/StockConcurrencyExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooper.stockconcurrencyexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockConcurrencyExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
